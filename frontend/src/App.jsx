f61995369559b310e9f62e12753110b396fc97ef
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2024-04-13 15:54:51 +0200

    Replace custom components and design with mui ones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,33 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+} from '@mui/material'
 import { TodoLists } from './todos/components/TodoLists'
-
-const MainAppBar = () => {
-  return (
-    <AppBar position='static' color='primary'>
-      <Toolbar>
-        <Typography variant='h6' color='inherit'>
-          Things to do
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  )
-}
-
-const mainWrapperStyle = { display: 'flex', flexDirection: 'column' }
-const centerContentWrapper = { display: 'flex', justifyContent: 'center' }
-const contentWrapperStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  maxWidth: '80rem',
-  flexGrow: 1,
-}
-const MainWrapper = ({ children }) => {
-  return (
-    <div style={mainWrapperStyle}>
-      <MainAppBar />
-      <div style={centerContentWrapper}>
-        <div style={contentWrapperStyle}>{children}</div>
-      </div>
-    </div>
-  )
-}
 
 const App = () => {
   return (
-    <MainWrapper>
-      <TodoLists style={{ margin: '1rem' }} />
-    </MainWrapper>
+    <Box displa="flex" flexDirection="column" >
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Typography variant='h6' color='inherit'>
+            Things to do
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box display="flex" justifyContent="center">
+        <Box
+          display="flex"
+          flexDirection="column"
+          maxWidth="80rem"
+          flexGrow={1}
+        >
+          <TodoLists style={{ margin: '1rem' }} />
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
